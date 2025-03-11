1969545e7ab1ed57e130ba031041ea1455363f73
thaissgarbosa <thais.ssgarbosa@gmail.com>
2025-03-11 11:38:06 -0300

    alteração: alterei o texto principal do index e minha conta

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function index(){
     return(
         <View>
-            <Text>gabriel</Text>
+            <Text>Julio</Text>
         </View>
     )
 }
--- app/(tabs)/minhaConta.tsx
+++ app/(tabs)/minhaConta.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function MinhaConta(){
     return(
         <View>
-            <Text>Minha Conta</Text>
+            <Text>nossa conta</Text>
         </View>
     )
 }
