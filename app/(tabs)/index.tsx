3465959bdc7bb9d21490370ac3cb001c19c131ac
thaissgarbosa <thais.ssgarbosa@gmail.com>
2025-03-11 11:28:42 -0300

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function index(){
     return(
         <View>
-            <Text>Explorar</Text>
+            <Text>gabriel</Text>
         </View>
     )
 }
