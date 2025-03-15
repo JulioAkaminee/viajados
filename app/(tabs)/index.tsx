a85c90ee9df679eacc74696c464be55bdf5bb67a
DevoluaP <devoluap@gmail.com>
2025-03-14 22:28:51 -0300

    criado componentes de voo e hotel

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,54 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView } from "react-native";
 
-export default function index(){
-    return(
-        <View>
-            <Text>Teste de commit</Text>
-            <View>
-                <Text>Teste de commit</Text>
-            </View>
-        </View>
-    )
+import BannerHotel from "@/components/BannerHotel";
+import BannerVoo from "@/components/BannerVoo";
+import BannerHotelFavoritos from "@/components/BannerHotelFavoritos";
+import BannerVooFavoritos from "@/components/BannerVooFavoritos";
+
+export default function index() {
+  return (
+    <ScrollView>
+      <BannerHotel
+        imagem={require("../../assets/images/hotel.jpg")}
+        nome="Hotel Maravilha"
+        avaliacao={5}
+        saida="14:00"
+        data="20 de Dezembro"
+        descricao="Um lindo hotel com vista para o mar"
+        preco="R$ 300,00"
+        onPress={() => console.log("Hotel selecionado!")}
+      />
+      <BannerVoo
+        imagem={require("../../assets/images/voo.jpg")}
+        destino="Rio de Janeiro"
+        origem="São Paulo"
+        descricao="Aproveite as belezas de copacabana"
+        saida="22:00"
+        data="10 de Maio"
+        preco="R$ 1.000,00"
+        onPress={() => console.log("Voo selecionado!")}
+      />
+      <BannerHotelFavoritos
+        imagem={require("../../assets/images/hotel.jpg")}
+        nome="Hotel Grandioso"
+        avaliacao={4}
+        saida="16:00"
+        data="08 de Junho"
+        descricao="Um lindo hotel com vista para o mar"
+        preco="R$ 450,00"
+        onPress={() => console.log("Hotel selecionado!")}
+      />
+      <BannerVooFavoritos
+        imagem={require("../../assets/images/voo.jpg")}
+        destino="Belo Horizonte"
+        origem="Salvador"
+        descricao="Conheça a incrível capital mineira"
+        saida="09:00"
+        data="31 de Outubro"
+        preco="R$ 870,00"
+        onPress={() => console.log("Voo selecionado!")}
+      />
+    </ScrollView>
+  );
 }
---
+++ components/BannerHotel.tsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  View,
+  Pressable,
+  Text,
+  Image,
+  ImageSourcePropType,
+} from "react-native";
+
+type Props = {
+  imagem: ImageSourcePropType;
+  nome: string;
+  avaliacao: number;
+  saida: string;
+  data: string;
+  descricao: string;
+  preco: string;
+  onPress: () => void;
+};
+
+export default function BannerHotel({
+  imagem,
+  nome,
+  avaliacao,
+  saida,
+  data,
+  descricao,
+  preco,
+  onPress,
+}: Props) {
+  const [favorito, setFavorito] = useState(false);
+
+  const favoritando = () => {
+    setFavorito((prev) => !prev);
+  };
+
+  const numeroEstrelas = (avaliacao: number) => {
+    const estrelas = [];
+    for (let i = 1; i <= 5; i++) {
+      estrelas.push(
+        <MaterialIcons
+          key={i}
+          name={i <= avaliacao ? "star" : "star-border"}
+          size={24}
+          color="#000"
+        />
+      );
+    }
+    return estrelas;
+  };
+
+  return (
+    <Pressable onPress={onPress} style={styles.container}>
+      <View style={{ position: "relative" }}>
+        <Pressable onPress={favoritando} style={styles.iconeFavorito}>
+          <MaterialIcons
+            name={favorito ? "favorite" : "favorite-border"}
+            size={24}
+            color={favorito ? "#D6005D" : "#000"}
+          />
+        </Pressable>
+        <Image source={imagem} style={styles.imagem} />
+      </View>
+      <Text style={styles.nome}>{nome}</Text>
+      <View style={styles.avaliacao}>{numeroEstrelas(avaliacao)}</View>
+      <Text style={styles.descricao}>{descricao}</Text>
+      <Text style={styles.saida}>Saída: {saida}</Text>
+      <Text style={styles.data}>Data: {data}</Text>
+      <Text style={styles.texto}>Preço por pessoa</Text>
+      <Text style={styles.preco}>{preco}</Text>
+      <Text style={[styles.texto, { marginBottom: 10 }]}>
+        Taxas e impostos não inclusos.
+      </Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 10,
+    overflow: "hidden",
+    backgroundColor: "#fff",
+    margin: 10,
+  },
+  imagem: {
+    width: "100%",
+    height: 150,
+  },
+  iconeFavorito: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    backgroundColor: "#fff",
+    borderStartEndRadius: 10,
+    padding: 4,
+    elevation: 5,
+    zIndex: 1,
+  },
+  nome: {
+    fontWeight: "bold",
+    textAlign: "left",
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 2,
+  },
+  avaliacao: {
+    flexDirection: "row",
+    marginHorizontal: 7,
+    marginBottom: 5,
+  },
+  descricao: {
+    textAlign: "left",
+    marginHorizontal: 10,
+    marginBottom: 5,
+  },
+  saida: {
+    textAlign: "left",
+    marginHorizontal: 10,
+    marginBottom: 5,
+  },
+  data: {
+    textAlign: "left",
+    marginHorizontal: 10,
+  },
+  texto: {
+    textAlign: "left",
+    marginHorizontal: 10,
+    marginTop: 5,
+  },
+  preco: {
+    fontSize: 17,
+    fontWeight: "bold",
+    textAlign: "left",
+    marginHorizontal: 10,
+  },
+});
