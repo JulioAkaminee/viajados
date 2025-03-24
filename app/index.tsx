d95396cb42b45068106a0025c41926ee7f67869c
Julio Akamine <95776461+JulioAkaminee@users.noreply.github.com>
2025-03-24 10:00:50 -0300

    Revert "alterações: tabs favoritos e inclusão de imagens"

--- app/index.tsx
+++ app/index.tsx
@@ -38,14 +38,6 @@ export default function Index() {
           value={email}
         />
 
-
-      {/* Link "Esqueceu a senha?" */}
-      <View style={styles.ContainerRecPass}>
-        <Link href="/(tabs)/explorar" style={styles.link}>
-          Esqueceu a senha?
-        </Link>
-      </View>
-
         {/* Campo de Senha */}
         <Input
           label="Digite sua Senha:"
@@ -54,7 +46,6 @@ export default function Index() {
           value={password}
           onChange={setPassword}
         />
-
 
         {/* Link "Esqueceu a senha?" */}
         <View style={styles.ContainerRecPass}>
